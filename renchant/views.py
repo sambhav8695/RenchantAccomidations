5befe8d85c815de1ece1c2e45e1e0238301870d0
sambhav8695 <50655927+sambhav8695@users.noreply.github.com>
2021-04-27 00:02:26 +0530

    Update views.py

--- renchant/views.py
+++ renchant/views.py
@@ -8,7 +8,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.conf import settings
-from captcha.fields import ReCaptchaField
 # Create your views here.
 
 
